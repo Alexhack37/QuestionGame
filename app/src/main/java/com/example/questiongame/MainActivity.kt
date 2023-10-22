84271390fb4c7c721dbad7a6e0e381ab24571f83
Alexhack37 <35271811+Alexhack37@users.noreply.github.com>
2023-10-22 13:05:34 +0200

    botono jugar
    boton test

--- app/src/main/java/com/example/questiongame/MainActivity.kt
+++ app/src/main/java/com/example/questiongame/MainActivity.kt
@@ -41,15 +41,6 @@ class MainActivity : ComponentActivity() {
 
                 SetUpNavGraph(navController = navController)
 
-                /*
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    //Greeting("Android")
-                    //TituloJuego("Askers!")
-                }*/
             }
         }
     }
